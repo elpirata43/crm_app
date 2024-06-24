2a21e0f127198f022e433e25472fbb159825f1b5
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-06-24 07:32:15 -0700

    removed seeders

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize) => {
     static associate(models) {
       User.hasMany(models.Account, {
         foreignKey: "ownerId",
-        // as: 'Account'
+        as: 'Account'
       });
     }
   }
