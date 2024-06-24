19462254f3a0ba241588f8f7a3edf69993f97e95
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-06-24 07:36:49 -0700

    added options to migrations

--- backend/db/migrations/20240212011634-create-account.js
+++ backend/db/migrations/20240212011634-create-account.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Accounts", {
+    await queryInterface.createTable(options, {
       id: {
         allowNull: false,
         autoIncrement: true,
